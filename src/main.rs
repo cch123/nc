30f55b5d120284d961ab556670cc7d204359bb3f
Xargin <buaa.cch@live.com>
2020-06-02 12:55:06 +0800

    basically done

--- src/main.rs
+++ src/main.rs
@@ -11,65 +11,65 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build()?;
 
     let body = c
-        .get("https://www.economist.com/finance-and-economics/2020/05/30/the-pandemic-could-lead-statisticians-to-change-how-they-estimate-gdp")
-        //.get("https://www.economist.com/printedition/2020-05-30")
+        //.get("https://www.economist.com/finance-and-economics/2020/05/30/the-pandemic-could-lead-statisticians-to-change-how-they-estimate-gdp")
+        .get("https://www.economist.com/printedition/2020-05-30")
         .send()
         .await?
         .text()
         .await?;
-    //dbg!(body);
-    parse(body.clone());
+
+    //parse(body.clone());
+    get_table(body.clone());
     Ok(())
 }
 
-fn parse(body: String) {
-    let html = r"
-        <DOCTYPE html>
-        <html>
-        <head></head>
-        <body>
-            <h1>Example</h1>
-            <p class='foo'>Hello, <li>fuck</li>world!</p>
-            <p class='foo'>I love HTML</p>
-        </body>
-        </html>
-    ";
+fn get_table(body : String) {
     let html = body.as_str();
-    //class="article__body-text"
+    let css_selector = ".list__item";
+
+    let doc = kuchiki::parse_html().one(html);
+
+    for list_item in doc.select(css_selector).unwrap() {
+        let list_item_node= list_item.as_node();
+        let mut title = String::new();
+        list_item_node.select(".list__title").unwrap().for_each(|t|{
+            let text = t.text_contents().clone();
+            title.push_str(text.as_str());
+        });
+        dbg!(title);
+        let mut item_arr = Vec::new();
+        list_item_node.select("a").unwrap().for_each(|t|{
+            let text = t.text_contents().clone();
+            let link = t.attributes.borrow().get("href").unwrap().to_string();
+            item_arr.push(text.clone());
+            item_arr.push(link.to_string());
+        });
+        dbg!(item_arr);
+    }
+}
+
+fn parse(body: String) {
+    let html = body.as_str();
     let css_selector = ".article__body-text";
 
     let document = kuchiki::parse_html().one(html);
 
     for css_match in document.select(css_selector).unwrap() {
-        // css_match is a NodeDataRef, but most of the interesting methods are
-        // on NodeRef. Let's get the underlying NodeRef.
         let as_node = css_match.as_node();
 
-        // In this example, as_node represents an HTML node like
-        //
-        //   <p class='foo'>Hello world!</p>"
-        //
-        // Which is distinct from just 'Hello world!'. To get rid of that <p>
-        // tag, we're going to get each element's first child, which will be
-        // a "text" node.
-        //
-        // There are other kinds of nodes, of course. The possibilities are all
-        // listed in the `NodeData` enum in this crate.
+        let mut content = String::new();
         as_node.children().for_each(|n| {
             match n.data() {
-                kuchiki::NodeData::Text(t) => println!("{:?}", t.clone().into_inner()),
-                kuchiki::NodeData::Element(e) => println!("{:?}", n.first_child().unwrap().as_text().unwrap().clone().into_inner()),
+                kuchiki::NodeData::Text(t) => {
+                    content.push_str(t.clone().into_inner().trim_matches(|c| c == '"'));
+                },
+                kuchiki::NodeData::Element(_) => {
+                    content.push_str(n.text_contents().as_str());
+                },
                 _ => println!("fuck"),
             }
         });
 
-        // Let's get the actual text in this text node. A text node wraps around
-        // a RefCell<String>, so we need to call borrow() to get a &str out.
-        //let text = text_node.as_text().unwrap();//.borrow();
-
-        // Prints:
-        //
-        //  "Hello, world!"
-        //  "I love HTML"
+        println!("{}\n", content);
     }
 }
